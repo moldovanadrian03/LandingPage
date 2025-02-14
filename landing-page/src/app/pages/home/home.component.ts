14396c71d34056d24a54eff9c9591c06036efe11
Adrian <moldovan.adrian.alexandru03@gmail.com>
2025-02-14 14:07:24 +0200

    Created features component and added it to the home page, styled it, made some global styling for icons, and card images.

--- landing-page/src/app/pages/home/home.component.ts
+++ landing-page/src/app/pages/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { AboutComponent } from "../../components/about/about.component";
 import { PeopleOpinionComponent } from '../../components/people-opinion/people-opinion.component';
+import { FeaturesComponent } from '../../components/features/features.component';
 
 @Component({
   selector: 'app-home',
   imports: [
     AboutComponent,
-    PeopleOpinionComponent
+    PeopleOpinionComponent,
+    FeaturesComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
